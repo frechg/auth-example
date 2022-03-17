b243cbd0f82913de33aba8c2a86e0c7e187f0783
frechg <galenfrechette@gmail.com>
2022-03-17 17:03:06 +0700

    Adds users and session request specs.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { "person@example.com" }
+    password { "testpass" }
     password_digest { "testpass" }
   end
 end
--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -2,11 +2,47 @@ require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
   describe "GET /authenticate" do
+    it "returns the sign in form" do
+      get "/authenticate"
+
+      expect(response).to have_http_status(200)
+    end
   end
 
   describe "POST /authenticate" do
+    it "authenticates an existing user" do
+      user = User.create(email: "person@example.com", password: "testpass")
+
+      post "/authenticate", params: {
+        user: {
+          email: "#{user.email}",
+          password: "testpass"
+        }
+      }
+
+      expect(response).to redirect_to("/")
+      follow_redirect!
+
+      expect(response).to have_http_status(200)
+      expect(response.body).to include("You are Authenticated")
+    end
   end
 
   describe "GET /logout" do
+    it "logs out the current user" do
+      # need an authenticated user
+      # should maybe test the current_user attibute?
+      #user = User.create(email: "person@example.com", password: "testpass")
+      #expect(current_user).to eq(user)
+
+      get "/logout"
+
+      expect(response).to redirect_to("/")
+      follow_redirect!
+
+      #expect(current_user).to be(nil)
+      expect(response).to have_http_status(200)
+      expect(response.body).to include("reAuth")
+    end
   end
 end
